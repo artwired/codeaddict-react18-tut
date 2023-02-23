7c3af33d553b7d08cff200e426fdd0f972ca25ad
Brad Cole <artwired@users.noreply.github.com>
2023-02-23 11:49:34 -0800

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import "./index.css";
 
 // function Greeting() {
 //   return <h2>My Component</h2>;
 // }
 function BookList() {
   return (
-    <section>
+    <section className="booklist">
       <Book />
       <Book />
       <Book />
@@ -16,20 +17,20 @@ function BookList() {
 }
 
 const Book = () => {
+  const title =
+    "Young Forever: The Secrets to Living Your Longest, Healthiest Life";
+  const author = "Dr. Mark Hyman MD";
   return (
-    <article>
-      <Image />
-      <Title />
-      <Author />
+    <article className="book">
+      <img
+        src="https://images-na.ssl-images-amazon.com/images/I/81dL13NFc3L._AC_UL900_SR900,600_.jpg"
+        alt="Young Forever: The Secrets to Living Your Longest, Healthiest Life"
+      />
+      <h2>{title}</h2>
+      <h4>{author}</h4>
     </article>
   );
 };
-
-const Image = () => <h2>Image Placeholder</h2>;
-const Title = () => {
-  return <h2>Book Title</h2>;
-};
-const Author = () => <h4>Author</h4>;
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
